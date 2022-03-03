396549e47109a2619142bb85329883e4d7faba6a
nnuriddin <nuriddindoniyorov2@gmail.com>
2022-03-03 21:05:24 +0500

    time project

---
+++ app.js
@@ -0,0 +1,27 @@
+let MyClockDisplay = document.getElementById("MyClockDisplay");
+
+function showTime(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let session = "AM";
+
+    if(hours == 0){
+        hours = 12;
+    }
+    if(hours > 12){
+        hours = hours - 12;
+        session = "PM";
+    }
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hours + ':' + minutes + ':' + seconds + ' ' + session;
+    MyClockDisplay.textContent = time;
+    setTimeout()
+    console.log(time);
+}
+
+showTime();
